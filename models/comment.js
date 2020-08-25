c45c33697066f67bb0a0a80be7462553e2e87bca
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-08-26 00:05:23 +0530

    added comment model

---
+++ models/comment.js
@@ -0,0 +1,16 @@
+//Schema For Comment
+var mongoose   = require("mongoose");
+
+var commentSchema = new mongoose.Schema({
+	text: String,
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User"
+		},
+		username: String
+	}
+});
+
+
+module.exports = mongoose.model("Comment", commentSchema);
