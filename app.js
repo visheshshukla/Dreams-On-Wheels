383acd7c7670431b2dafa57b32f04202cb127791
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-08-27 03:06:42 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 var express    		= require("express");
 var app        		= express();
 var bodyParser		= require("body-parser");
+var mongoose   		= require("mongoose");
+var passport   		= require("passport");
+var localStrategy	= require("passport-local"); 
+var methodOverride  = require("method-override");
+var flash    		= require("connect-flash");
+var rp				= require('request-promise');
+var cors            = require('cors');
 var Showroom   		= require("./models/showroom.js");
+var Comment    		= require("./models/comment.js");
+var User	    	= require("./models/user.js");
 var showroomRoutes  = require("./routes/showrooms.js");
+var commentRoutes   = require("./routes/comments.js");
+var userRoutes      = require("./routes/users.js");
+
+
+mongoose.connect("mongodb://localhost/Dreams_On_Wheels",{
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false });
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+app.use(cors());
 
 
 
 //Routes
 app.use(showroomRoutes);
+app.use(commentRoutes);
+app.use(userRoutes);
 
 
 app.listen(3000,function() {
