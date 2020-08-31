acb3ab744ee89c7442cbdec78867306e50a856b8
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-08-31 17:39:15 +0530

    stylesheet fix

--- app.js
+++ app.js
@@ -21,7 +21,8 @@ useUnifiedTopology: true,
 useFindAndModify: false });
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride("_method"));
 app.use(flash());
 app.use(cors());
